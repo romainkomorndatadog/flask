c585eb93d4bb4837b0354f2108937a99e686edbc
Romain Komorn <romain.komorn@datadoghq.com>
2023-08-16 15:33:59 +0100

    non-empty commit for bump

--- src/flask/json/tag.py
+++ src/flask/json/tag.py
@@ -55,7 +55,6 @@ from werkzeug.http import parse_date
 from ..json import dumps
 from ..json import loads
 
-# blahlhbalh
 
 class JSONTag:
     """Base class for defining type tags for :class:`TaggedJSONSerializer`."""
