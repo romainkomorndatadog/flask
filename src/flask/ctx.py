c5d8e8086ebd06c21d61fa1d7441c3997d10cab0
Romain Komorn <romain.komorn@datadoghq.com>
2023-08-16 15:37:54 +0100

    non-empty commit for bump

--- src/flask/ctx.py
+++ src/flask/ctx.py
@@ -19,7 +19,6 @@ if t.TYPE_CHECKING:  # pragma: no cover
     from .sessions import SessionMixin
     from .wrappers import Request
 
-# hellooo
 
 # a singleton sentinel value for parameter defaults
 _sentinel = object()
