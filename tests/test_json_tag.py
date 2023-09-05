e9522e4629d45cf08b5f8585d7cdf7bbcb10ce94
Romain Komorn <romain.komorn@datadoghq.com>
2023-09-05 08:38:23 +0100

    updated reason

--- tests/test_json_tag.py
+++ tests/test_json_tag.py
@@ -8,8 +8,8 @@ from markupsafe import Markup
 from flask.json.tag import JSONTag
 from flask.json.tag import TaggedJSONSerializer
 
-ITR_TEST_UNSKIPPABLE_REASON = "datadog_itr_test_unskippable" 
-DATADOG_ITR_SUITE_UNSKIPPABLE = False
+ITR_TEST_UNSKIPPABLE_REASON = "datadog_itr_unskippable" 
+DATADOG_ITR_UNSKIPPABLE = False
 
 @pytest.mark.parametrize(
     "data",
